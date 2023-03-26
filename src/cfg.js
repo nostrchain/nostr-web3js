19f86c78b5803b8ac87e64f3e98c99f2a5a3df26
unknown <nostrchain@gmail.com>
2023-03-27 02:15:22 +0800

    codeing

--- src/cfg.js
+++ src/cfg.js
@@ -6,6 +6,6 @@ export const INFURA_ID = '9000';
 // EVM chain remote call RPC
 //export const JSON_RPC_PROVIDER = "http://64.176.44.80:8545";
 //export const JSON_RPC_PROVIDER = "http://192.168.18.101:8545";
-export const JSON_RPC_PROVIDER = "https://testrpc.nostrchain.xyz:8545/";
+export const JSON_RPC_PROVIDER = "https://testrpc.nostrchain.xyz";
 // NostrChain funds storage security contract, already open source, no changes required
 export const NPUB_BANK_CONTRACT = "0xE65F9E40Bd25EE5323F7B05B9AE89fcf8e14cA16";  
